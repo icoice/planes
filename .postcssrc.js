2ecb477248ef0e0dc9bd0d9018b9f1555b43ec67
icoice <icoice@icoicedeMacBook-Pro.local>
2018-02-23 15:12:41 +0800

    update

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    "postcss-easy-import": {},
+    "postcss-import": {},
+    "postcss-url": {},
+    "postcss-define-units": {},
+    "postcss-mixins": {},
+    "precss": {},
+    "postcss-calc": {},
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {}
+  }
+}
