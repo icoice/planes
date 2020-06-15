77402c46f5b2be05afe172e8841e9058a2fbea01
root <985611106@qq.com>
2020-06-15 11:30:25 +0800

    update

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,15 +1,15 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
+  parser: 'postcss-scss',
   "plugins": {
     "postcss-easy-import": {},
     "postcss-import": {},
     "postcss-url": {},
     "postcss-lilia-units": {},
     "postcss-mixins": {},
-    "precss": {},
     "postcss-calc": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {}, // to edit target browsers: use "browserslist" field in package.json
+    "precss": {},
   }
 }
