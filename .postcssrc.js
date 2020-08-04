837f269067a21a5987f0e04529efeb16f27ee2a1
root <985611106@qq.com>
2020-08-04 17:24:11 +0800

    update

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,15 +1,16 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
-
 module.exports = {
   parser: 'postcss-scss',
-  "plugins": {
+  // 注意这里插件排序，它按排序执行编译，请不要随意改变次序
+  plugins: {
+    "postcss-preset-env": {},
     "postcss-easy-import": {},
     "postcss-import": {},
     "postcss-url": {},
     "postcss-lilia-units": {},
     "postcss-mixins": {},
-    "postcss-calc": {},
     "autoprefixer": {}, // to edit target browsers: use "browserslist" field in package.json
     "precss": {},
+    "postcss-calc": {},
   }
 }
